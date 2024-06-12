c470365aedf7ae9be714a9939f08590d84f0a61d
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-06-12 11:51:33 -0300

    1

---
+++ src/createWallet.js
@@ -0,0 +1,24 @@
+const  bip32 = require('bip32')
+const  bip39 = require('bip39')
+const  bitcoin = require('bitcoinjs-lib')
+
+//Definir Rede
+const network = bitcoin.networks.testnet
+
+//Derivação Carteiras HD
+const path = `m/49'/1'/0'/0`
+
+//Criando o MNEMONIC para a seed 'PALAVRAS DE SENHA'
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+//Criando a raiz da carteira HD
+let root = bip32.fromSeed(seed, network)
+
+//Criando uma conta - par pvt-pub-keys
+let account = root.derivePath(path)
+
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network
+}).address
